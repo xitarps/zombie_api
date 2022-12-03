1ebc420871bb0375c83c94f79afe16363ca3d224
Rodrigo - Xita rps <xitarps@gmail.com>
2022-12-03 18:52:01 -0300

    [Feat]: Add ApiController: api/v1/controllers centralizer

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Main controller for v1 api - all controller inside api::v1 inherits from this controller
+class Api::V1::ApiController < ApplicationController
+end

e46c5412651615ecf03f6c5a14cd65029c52c9fb
Rodrigo - Xita rps <xitarps@gmail.com>
2022-12-03 17:05:40 -0300

    [Feat]: Add Survivor model

---
+++ db/migrate/20221203200016_create_survivors.rb
@@ -0,0 +1,10 @@
+class CreateSurvivors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :survivors, id: :uuid do |t|
+      t.string :name
+      t.integer :gender
+
+      t.timestamps
+    end
+  end
+end
